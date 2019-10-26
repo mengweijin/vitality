4693941bd643c2efdba130382a46c1d94edf6e24
mengweijin <1002284406@qq.com>
2019-10-26 22:13:57 +0800

    comments

--- mwjwork-framework/src/main/java/com/mwj/mwjwork/framework/validator/CharsetLengthValidator.java
+++ mwjwork-framework/src/main/java/com/mwj/mwjwork/framework/validator/CharsetLengthValidator.java
@@ -14,6 +14,9 @@ import java.nio.charset.Charset;
  * 当数据库长度限制为10字节，数据库编码为UTF-8(一个汉字占三个字节)，此时数据库只能存储最多3个汉字，而@Length注解只会判断java中字符串的长度，
  * 当传入字符串：“四个汉字”，@Length(max=10)注解判断长度为4，校验通过，而数据库中，四个汉字的字节长度为：3*4=12，此时插入数据会发生SQL执行异常，
  * 所以需要自定义这个注解，自定义validator.
+ *
+ * 出现以上问题的原因是因为在创建表字段时长度用varchar(20 byte), 添加了byte表名该字段是以字节长度计算，一般情况下要严格避免使用byte,
+ * 可以直接使用varchar(20)来定义字段长度，这样就是以字符长度来计算，就不会出现以上问题。
  * @date Create in 2019-10-21 21:06
  **/
 public class CharsetLengthValidator implements ConstraintValidator<CharsetLength, CharSequence> {

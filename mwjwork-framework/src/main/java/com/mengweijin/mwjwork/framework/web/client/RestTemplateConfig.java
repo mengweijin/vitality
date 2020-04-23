6e198ef8c17d8c38043a5d25ac94d6514a6d8277
mengweijin <1002284406@qq.com>
2020-04-23 23:57:53 +0800

    add RestTemplate config

---
+++ mwjwork-framework/src/main/java/com/mengweijin/mwjwork/framework/web/client/RestTemplateConfig.java
@@ -0,0 +1,19 @@
+package com.mengweijin.mwjwork.framework.web.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return restTemplateBuilder.build();
+    }
+}

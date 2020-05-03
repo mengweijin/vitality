f656bae0e3c9f6b8af6c83868a66a02d5dfb1ad3
mengweijin <1002284406@qq.com>
2020-05-03 18:06:07 +0800

    MetaObjectHandler

--- mwjwork-mybatis-plus/src/main/java/com/mengweijin/mwjwork/mybatis/DefaultMetaObjectHandler.java
+++ mwjwork-mybatis-plus/src/main/java/com/mengweijin/mwjwork/mybatis/DefaultMetaObjectHandler.java
@@ -19,13 +19,13 @@ public class DefaultMetaObjectHandler implements MetaObjectHandler {
     public void insertFill(MetaObject metaObject) {
         this.strictInsertFill(metaObject, LambdaWrapper.getFieldName(BaseEntity::getCreateTime), LocalDateTime.class, LocalDateTime.now());
         this.strictInsertFill(metaObject, LambdaWrapper.getFieldName(BaseEntity::getCreateBy), String.class, SYSTEM);
-        this.strictUpdateFill(metaObject, LambdaWrapper.getFieldName(BaseEntity::getUpdateTime), LocalDateTime.class, LocalDateTime.now());
-        this.strictUpdateFill(metaObject, LambdaWrapper.getFieldName(BaseEntity::getUpdateBy), String.class, SYSTEM);
+        this.strictInsertFill(metaObject, LambdaWrapper.getFieldName(BaseEntity::getUpdateTime), LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, LambdaWrapper.getFieldName(BaseEntity::getUpdateBy), String.class, SYSTEM);
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        this.strictUpdateFill(metaObject, LambdaWrapper.getFieldName(BaseEntity::getUpdateTime), LocalDateTime.class, LocalDateTime.now());
-        this.strictUpdateFill(metaObject, LambdaWrapper.getFieldName(BaseEntity::getUpdateBy), String.class, SYSTEM);
+        this.setFieldValByName(LambdaWrapper.getFieldName(BaseEntity::getUpdateTime), LocalDateTime.now(), metaObject);
+        this.setFieldValByName(LambdaWrapper.getFieldName(BaseEntity::getUpdateBy), SYSTEM, metaObject);
     }
 }

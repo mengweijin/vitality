8c4c287dfb390dd1e162f8925044765b673c87d7
mengweijin <1002284406@qq.com>
2020-05-06 22:54:37 +0800

    update port

--- mwjwork-vue-ui/config/dev.env.js
+++ mwjwork-vue-ui/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
-    API_ROOT: '"http://localhost:8080"'
+    API_ROOT: '"http://localhost:8001"'
 })

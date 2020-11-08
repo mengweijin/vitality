4d76e78818948b5ee6bfd83688b1c8874d64de81
mengweijin <1002284406@qq.com>
2020-11-08 12:21:41 +0800

    H2 Server

---
+++ quickboot-web-jpa-sample/src/main/java/com/github/mengweijin/quickboot/sample/H2Server.java
@@ -0,0 +1,33 @@
+package com.github.mengweijin.quickboot.sample;
+
+import lombok.extern.slf4j.Slf4j;
+import org.h2.tools.Server;
+
+import java.sql.SQLException;
+
+@Slf4j
+public class H2Server {
+
+    private static final String port = "9999";
+    private static Server server;
+
+    public static void start() throws SQLException {
+        server = Server.createTcpServer(new String[]{"-tcp", "-tcpAllowOthers", "-ifNotExists", "-tcpPort", port})
+                .start();
+        log.info(server.getURL());
+    }
+
+    public static void stop() {
+        if (server != null) {
+            server.stop();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            H2Server.start();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}

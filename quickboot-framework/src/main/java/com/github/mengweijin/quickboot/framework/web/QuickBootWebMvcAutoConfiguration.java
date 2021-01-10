96c8db49f8216dc6cde94bfb5c51e2be60cfa365
mengweijin <1002284406@qq.com>
2021-01-10 17:26:53 +0800

    Version 1.0.18

--- quickboot-framework/src/main/java/com/github/mengweijin/quickboot/framework/web/QuickBootWebMvcAutoConfiguration.java
+++ quickboot-framework/src/main/java/com/github/mengweijin/quickboot/framework/web/QuickBootWebMvcAutoConfiguration.java
@@ -1,11 +1,13 @@
 package com.github.mengweijin.quickboot.framework.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,6 +24,20 @@ public class QuickBootWebMvcAutoConfiguration implements WebMvcConfigurer {
 
     @Autowired
     private PagerArgumentResolver pagerArgumentResolver;
+
+    @Value("${quickboot.cors: false}")
+    private boolean cors;
+
+    /**
+     * 跨域
+     * @param registry CorsRegistry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        if(cors) {
+            registry.addMapping("/**");
+        }
+    }
 
     /**
      * 注册参数解析器

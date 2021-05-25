46f3dd79558d750a7a8bc7708e889c0c1fc4f830
mengweijin <1002284406@qq.com>
2021-05-25 19:21:44 +0800

    IdUtils

--- quickboot-framework/src/main/java/com/github/mengweijin/quickboot/framework/util/IdUtils.java
+++ quickboot-framework/src/main/java/com/github/mengweijin/quickboot/framework/util/IdUtils.java
@@ -41,17 +41,17 @@ public class IdUtils extends IdUtil {
 	 * 它的含义是：线程开始执行同步代码块之前，必须先获得对同步代码块的锁定。
 	 * 任何时刻只能有一个线程可以获得对同步监视器的锁定，当同步代码块执行完成后，该线程会释放对该同步监视器的锁定。
 	 */
-	private static final String TIMESTAMP_ID = "TIMESTAMP_ID";
+	private static final Object LOCK = new Object();
 
 	/**
 	 * 时间戳ID
+	 * TODO synchronized 存在高并发下的性能问题，考虑使用 Lock, cas 等机制来处理
 	 * @return
 	 */
 	public static String timestampId() {
-		synchronized(TIMESTAMP_ID){
+		synchronized(LOCK){
 			return DateFormatUtils.format(new Date(), DatePattern.PURE_DATETIME_MS_PATTERN);
 		}
-
 	}
 
 }
--- quickboot-framework/src/main/java/com/github/mengweijin/quickboot/framework/web/QuickBootWebMvcAutoConfiguration.java
+++ quickboot-framework/src/main/java/com/github/mengweijin/quickboot/framework/web/QuickBootWebMvcAutoConfiguration.java
@@ -2,17 +2,10 @@ package com.github.mengweijin.quickboot.framework.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.List;
 
 /**

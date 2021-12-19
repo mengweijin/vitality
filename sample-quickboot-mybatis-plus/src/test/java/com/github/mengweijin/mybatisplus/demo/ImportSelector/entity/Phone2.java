2518995cb9774bcf78e1ac5ca5669868c0c72146
mengweijin <1002284406@qq.com>
2021-12-19 16:42:04 +0800

    Release version 1.0.31

--- sample-quickboot-mybatis-plus/src/test/java/com/github/mengweijin/mybatisplus/demo/ImportSelector/entity/Phone2.java
+++ sample-quickboot-mybatis-plus/src/test/java/com/github/mengweijin/mybatisplus/demo/ImportSelector/entity/Phone2.java
@@ -1,6 +1,6 @@
 package com.github.mengweijin.mybatisplus.demo.ImportSelector.entity;
 
-import com.github.mengweijin.quickboot.mybatis.BaseEntity;
+import com.github.mengweijin.quickboot.mybatis.entity.BaseEntity;
 import lombok.Data;
 
 @Data

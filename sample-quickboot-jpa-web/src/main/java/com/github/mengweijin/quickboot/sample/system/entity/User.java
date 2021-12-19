b5aca91abed84a43a95092d4c6fc31f7e31b76b9
mengweijin <1002284406@qq.com>
2021-12-19 16:21:50 +0800

    refactor

--- sample-quickboot-jpa-web/src/main/java/com/github/mengweijin/quickboot/sample/system/entity/User.java
+++ sample-quickboot-jpa-web/src/main/java/com/github/mengweijin/quickboot/sample/system/entity/User.java
@@ -4,13 +4,10 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.github.mengweijin.quickboot.jpa.BaseEntity;
 import com.github.mengweijin.quickboot.sample.system.enums.Role;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import lombok.experimental.Accessors;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -24,7 +21,6 @@ import javax.validation.constraints.NotBlank;
  * @description
  * @date Create in 2019-07-27 12:49
  **/
-@ApiModel(description = "用户")
 @Data
 @Entity
 @Accessors(chain = true)
@@ -32,7 +28,6 @@ import javax.validation.constraints.NotBlank;
 @ToString(callSuper = true)
 public class User extends BaseEntity {
 
-    @ApiModelProperty(value = "用户名称", example = "张三")
     @NotBlank
     @Column(length = 30)
     private String name;

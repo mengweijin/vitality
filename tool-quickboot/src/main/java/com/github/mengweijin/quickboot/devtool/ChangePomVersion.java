51bb55206ccabbaf474b023d25a8d9029a182816
mengweijin <1002284406@qq.com>
2022-02-27 16:17:48 +0800

    upgrade library version

--- tool-quickboot/src/main/java/com/github/mengweijin/quickboot/devtool/ChangePomVersion.java
+++ tool-quickboot/src/main/java/com/github/mengweijin/quickboot/devtool/ChangePomVersion.java
@@ -23,9 +23,9 @@ public class ChangePomVersion {
 
     private static final String POM_XML = "pom.xml";
 
-    private static final String OLD_VERSION = "1.0.38";
+    private static final String OLD_VERSION = "1.0.39-SNAPSHOT";
 
-    private static final String NEW_VERSION = "1.0.39-SNAPSHOT";
+    private static final String NEW_VERSION = "1.0.39";
 
     @SneakyThrows
     public static void main(String[] args) {

a433fa19e04966e29b3fb4a918f21883f0db9423
mengweijin <1002284406@qq.com>
2022-03-26 23:47:04 +0800

    layui

---
+++ sample-quickboot-layui/src/main/java/com/github/mengwejin/layui/DemoLayuiApplication.java
@@ -0,0 +1,16 @@
+package com.github.mengwejin.layui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mengweijin
+ * @date 2022/3/26
+ */
+@SpringBootApplication
+public class DemoLayuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoLayuiApplication.class, args);
+    }
+}

2a91d2ac75395fb4cf5d419c1c7da515975d2099
mengweijin <1002284406@qq.com>
2022-03-26 18:18:43 +0800

    1.0.41-SNAPSHOT

--- tool-quickboot/src/main/java/com/github/mengweijin/quickboot/devtool/ChangePomVersion.java
+++ tool-quickboot/src/main/java/com/github/mengweijin/quickboot/devtool/ChangePomVersion.java
@@ -23,9 +23,9 @@ public class ChangePomVersion {
 
     private static final String POM_XML = "pom.xml";
 
-    private static final String OLD_VERSION = "1.0.41-SNAPSHOT";
+    private static final String OLD_VERSION = "1.0.41";
 
-    private static final String NEW_VERSION = "1.0.41";
+    private static final String NEW_VERSION = "1.0.42-SNAPSHOT";
 
     @SneakyThrows
     public static void main(String[] args) {

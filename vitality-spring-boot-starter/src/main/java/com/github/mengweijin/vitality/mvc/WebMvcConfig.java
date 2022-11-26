b1ede8c00456173ed90aadbbece126c064db0c5d
mengweijin <1002284406@qq.com>
2022-11-26 17:54:38 +0800

    generator

--- vitality-spring-boot-starter/src/main/java/com/github/mengweijin/vitality/mvc/WebMvcConfig.java
+++ vitality-spring-boot-starter/src/main/java/com/github/mengweijin/vitality/mvc/WebMvcConfig.java
@@ -7,8 +7,10 @@ import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -43,4 +45,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
             factory.addErrorPages(errorPage404);
         });
     }
+
+    /**
+     * debug 模式不缓存静态资源
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if(vitalityProperties.isDebug()) {
+            registry.addResourceHandler("/**").addResourceLocations("classpath:/static/").setCacheControl(CacheControl.noStore());
+        }
+    }
+
 }

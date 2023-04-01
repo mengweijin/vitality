a3fa852404448193aaf120e4a1f7e6ef62d70795
mengweijin <1002284406@qq.com>
2023-04-01 21:39:12 +0800

    V1.3.0-SNAPSHOT

--- vitality-framework/src/main/java/com/github/mengweijin/vitality/framework/minio/MinioService.java
+++ vitality-framework/src/main/java/com/github/mengweijin/vitality/framework/minio/MinioService.java
@@ -19,7 +19,6 @@ import io.minio.messages.Bucket;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.time.LocalDate;
 import java.util.List;
 

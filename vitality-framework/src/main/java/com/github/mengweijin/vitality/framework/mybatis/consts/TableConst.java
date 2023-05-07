7c304086d487cf0b048769f04b7c763fd72ba436
mengweijin <1002284406@qq.com>
2023-05-07 18:14:43 +0800

    table const

--- vitality-framework/src/main/java/com/github/mengweijin/vitality/framework/mybatis/consts/TableConst.java
+++ vitality-framework/src/main/java/com/github/mengweijin/vitality/framework/mybatis/consts/TableConst.java
@@ -6,10 +6,10 @@ package com.github.mengweijin.vitality.framework.mybatis.consts;
  */
 public interface TableConst {
 
-    String SYS_USER = "SYS_USER";
-    String SYS_DEPT = "SYS_DEPT";
-    String SYS_ROLE = "SYS_ROLE";
-    String SYS_DEPT_USER_RLT = "SYS_DEPT_USER_RLT";
-    String SYS_ROLE_USER_RLT = "SYS_ROLE_USER_RLT";
+    String SYS_USER = "VTL_USER";
+    String SYS_DEPT = "VTL_DEPT";
+    String SYS_ROLE = "VTL_ROLE";
+    String SYS_DEPT_USER_RLT = "VTL_DEPT_USER_RLT";
+    String SYS_ROLE_USER_RLT = "VTL_ROLE_USER_RLT";
 
 }

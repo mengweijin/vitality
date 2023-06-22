fdbde3febb97c2e2abb54d144bd4817d988a9d2f
mengweijin <1002284406@qq.com>
2023-06-22 15:54:25 +0800

    add rlt service

--- vitality-framework/src/main/java/com/github/mengweijin/vitality/system/service/VtlRoleService.java
+++ vitality-framework/src/main/java/com/github/mengweijin/vitality/system/service/VtlRoleService.java
@@ -4,7 +4,9 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.github.mengweijin.vitality.system.dto.VtlRoleDTO;
 import com.github.mengweijin.vitality.system.entity.VtlRole;
+import com.github.mengweijin.vitality.system.entity.VtlUserRoleRlt;
 import com.github.mengweijin.vitality.system.mapper.VtlRoleMapper;
+import org.dromara.hutool.core.collection.CollUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +23,8 @@ public class VtlRoleService extends ServiceImpl<VtlRoleMapper, VtlRole> {
 
     @Autowired
     private VtlRoleMapper vtlRoleMapper;
+    @Autowired
+    private VtlUserRoleRltService vtlUserRoleRltService;
 
     public VtlRoleDTO detailById(Long id) {
         return vtlRoleMapper.detailById(id);
@@ -36,6 +40,28 @@ public class VtlRoleService extends ServiceImpl<VtlRoleMapper, VtlRole> {
 
     public List<VtlRoleDTO> getByUserId(Long userId) {
         return vtlRoleMapper.getByUserId(userId);
+    }
+
+    public void addUsers(Long roleId, List<Long> userIdList) {
+        if(CollUtil.isEmpty(userIdList)) {
+            return;
+        }
+        List<VtlUserRoleRlt> alreadyExistedRltList = vtlUserRoleRltService.lambdaQuery().eq(VtlUserRoleRlt::getRoleId, roleId).in(VtlUserRoleRlt::getUserId, userIdList).list();
+        List<Long> alreadyExistedUserIdList = alreadyExistedRltList.stream().map(VtlUserRoleRlt::getUserId).toList();
+
+        List<VtlUserRoleRlt> userRoleRltList = userIdList.stream()
+                .filter(userId -> alreadyExistedUserIdList.stream().noneMatch(existed -> existed.equals(userId)))
+                .map(userId -> {
+                    VtlUserRoleRlt rlt = new VtlUserRoleRlt();
+                    rlt.setUserId(userId);
+                    rlt.setRoleId(roleId);
+                    return rlt;
+                })
+                .toList();
+
+        if (CollUtil.isNotEmpty(userRoleRltList)) {
+            vtlUserRoleRltService.saveBatch(userRoleRltList);
+        }
 
     }
 }

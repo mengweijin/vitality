d04bb076d30a80c01844e6c07de6d341ff88815c
mengweijin <1002284406@qq.com>
2024-07-09 23:55:56 +0800

    main

--- vitality-admin/src/main/java/com/github/mengweijin/vitality/admin/VitalityAdminApplication.java
+++ vitality-admin/src/main/java/com/github/mengweijin/vitality/admin/VitalityAdminApplication.java
@@ -1,15 +1,22 @@
 package com.github.mengweijin.vitality.admin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author mengweijin
  */
+@Slf4j
 @SpringBootApplication
 public class VitalityAdminApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VitalityAdminApplication.class, args);
+		try {
+			SpringApplication.run(VitalityAdminApplication.class, args);
+			log.info("SYSTEM STARTUP SUCCESS");
+		} catch (Exception e) {
+			log.info("SYSTEM STARTUP FAILED");
+		}
 	}
 }

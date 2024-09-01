992879d70173f1ea6def9b5e040c32ec9000adbc
mengweijin <1002284406@qq.com>
2024-09-01 14:43:03 +0800

    home page.

--- vitality-ui/src/views/welcome/index.vue
+++ vitality-ui/src/views/welcome/index.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
+import avatar from "@/assets/login/avatar.svg?component";
+
 defineOptions({
   name: "Welcome"
 });
 </script>
 
 <template>
-  <h1>Vitality Admin</h1>
+  <div class="container">
+    <div class="picture">
+      <avatar class="avatar" />
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  height: 100%;
+}
+.picture {
+  width: 100%;
+  height: 80%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.avatar {
+  width: 40%;
+  height: 40%;
+}
+</style>

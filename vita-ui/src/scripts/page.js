a64fe2c305e730e0f3b74084d8c7952d6a3bde3d
mengweijin <1002284406@qq.com>
2025-03-22 18:42:55 +0800

    ui: screenfull

--- vita-ui/src/scripts/page.js
+++ vita-ui/src/scripts/page.js
@@ -19,7 +19,7 @@ let page = {
     let config = {
       id: `tab-${new Date()}`,
       // 此处加 n 仅为演示区分，实际应用不需要
-      title: "New Tab",
+      title: "NewTab",
       content: `<div id="${this.id}"></div>`,
       done: (params) => {
         // 给新标签头添加上下文菜单
@@ -44,32 +44,47 @@ let page = {
       trigger: "contextmenu",
       data: [
         {
+          title: '刷新',
+          action: 'refresh'
+        }, {
+          type: '-'
+        },
+        {
           title: "关闭当前",
+          action: 'close',
           mode: "this",
         },
         {
           title: "关闭右侧",
+          action: 'close',
           mode: "right",
         },
         {
           title: "关闭其他",
+          action: 'close',
           mode: "other",
         },
         {
           title: "关闭所有",
+          action: 'close',
           mode: "all",
         },
       ],
       click: function (data, othis, event) {
         // 获取活动标签索引
         let index = this.elem.index();
-        if (data.mode === "this") {
-          // 关闭当前标签
-          tabs.close("tabs", index);
-        } else {
-          // 批量关闭标签
-          tabs.closeMult("tabs", data.mode, index);
+        if(data.action === "refresh") {
+          tabs.refresh(`${ID}`);
+        } else if(data.action === "close") {
+          if (data.mode === "this") {
+            // 关闭当前标签
+            tabs.close("tabs", index);
+          } else {
+            // 批量关闭标签
+            tabs.closeMult("tabs", data.mode, index);
+          }
         }
+        
       },
     };
     return opts;
